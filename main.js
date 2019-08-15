ea83e87ae0bfaa2838af07fa0f9594b64acf7ed2
andrparsons <andrparsons@gmail.com>
2019-08-14 23:36:28 -0400

    added menu navigation fuctionallity

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 // DOM secltions
 
 const menuBtn = document.querySelector(".menu-btn");
+const overlay = document.querySelector(".overlay");
+const links = document.querySelector(".links");
 
 //initial state
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
+    overlay.classList.add("active");
+    links.classList.add("active");
 
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
+    overlay.classList.remove("active");
+    links.classList.remove("active");
 
     showMenu = false;
   }
